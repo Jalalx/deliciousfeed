acd260f979617fc700b679e0f63f92d54241566b
jalalx <sjamini@gmail.com>
2017-04-02 21:17:45 +0430

    mini refactor

--- Controllers/BotController.cs
+++ Controllers/BotController.cs
@@ -11,26 +11,10 @@ namespace deliciousfeed.Controllers
 {
     public class BotController : Controller
     {
-        private static readonly List<string> _logs = new List<string>();
-
         [HttpGet]
         public IActionResult Index()
         {
-            return Ok($"OK\r\n{DateTime.Now}");
-        }
-
-        [HttpGet]
-        public IActionResult Logs()
-        {
-            var result = string.Join("\r\n", _logs);
-            if (result.Any())
-            {
-                return Ok(result);
-            }
-            else
-            {
-                return Ok("No logs to display.");
-            }
+            return Ok($"{DateTime.Now}\tOK");
         }
 
         [HttpPost]
@@ -38,30 +22,18 @@ namespace deliciousfeed.Controllers
         {
             var message = update.Message;
 
-            var logMessage = string.Format("Received Message from {0}", message.Chat.Id);
-            Console.WriteLine(logMessage);
-            _logs.Add(logMessage);
+            Console.WriteLine(string.Format("Received Message from {0}", message.Chat.Id));
 
             if (message.Type == MessageType.TextMessage)
             {
-                _logs.Add(string.Format("Received Message: {0}", message.Text));
+                Console.WriteLine(string.Format("Received Message: {0}", message.Text));
 
                 // Echo each Message
                 await TelegramBotConfig.Client.SendTextMessageAsync(message.Chat.Id, message.Text);
             }
-            else if (message.Type == MessageType.PhotoMessage)
+            else
             {
-                // Download Photo
-                var file = await TelegramBotConfig.Client.GetFileAsync(message.Photo.LastOrDefault()?.FileId);
-
-                var filename = file.FileId + "." + file.FilePath.Split('.').Last();
-
-                using (var saveImageStream = System.IO.File.Open(filename, FileMode.Create))
-                {
-                    await file.FileStream.CopyToAsync(saveImageStream);
-                }
-
-                await TelegramBotConfig.Client.SendTextMessageAsync(message.Chat.Id, "Thx for the Pics");
+                Console.WriteLine($"Received Message Type: {message.Type}");
             }
 
             return Ok();
