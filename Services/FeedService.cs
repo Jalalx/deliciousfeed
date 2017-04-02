909b92eda42ebf10c736886bd1619f232a5cc786
jalalx <sjamini@gmail.com>
2017-04-02 22:59:22 +0430

    some improvments

--- Services/FeedService.cs
+++ Services/FeedService.cs
@@ -2,6 +2,6 @@ namespace deliciousfeed
 {
     public class FeedService
     {
-        
+        private const string ConnectionStringVariableName = "MongoDbConnectionString";
     }
 }
--- TelegramBot.cs
+++ TelegramBot.cs
@@ -1,3 +1,4 @@
+using System;
 using System.IO;
 using Telegram.Bot;
 
@@ -9,8 +10,22 @@ namespace deliciousfeed
 
         static TelegramBotConfig()
         {
-            var filePath = Path.Combine(Directory.GetCurrentDirectory(), @"Secrets\Telegram-bot");
-            var key = File.ReadAllText(filePath);
+            var key = Environment.GetEnvironmentVariable("TelegramBotKey");
+            if (string.IsNullOrEmpty(key))
+            {
+                // if environment variable was not found, try reading from file.
+                var filePath = Path.Combine(Directory.GetCurrentDirectory(), @"Secrets\Telegram-bot");
+                if (File.Exists(filePath))
+                {
+                    key = File.ReadAllText(filePath);
+                }
+            }
+
+            if (string.IsNullOrEmpty(key))
+            {
+                throw new InvalidOperationException("Telegram Bot Key is not specified.");
+            }
+            
             Client = new TelegramBotClient(key);
         }
     }
